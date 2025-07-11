2fd1bf7e542db67770bf33b066efc18ff78ed06c
Sgamboa567 <sgamboa765@gmail.com>
2025-07-10 22:29:12 -0500

    Ajustes de metadata, menú y mejoras en navegación

--- src/app/metadata.ts
+++ src/app/metadata.ts
@@ -6,7 +6,13 @@ export const metadata: Metadata = {
     template: '%s | PowerMK',
     default: 'PowerMK - Sistema de Gestión Mary Kay',
   },
+  metadataBase: new URL('https://power-mk.vercel.app/'),
   description: 'Sistema de Gestión Mary Kay',
+  openGraph: {
+    title: 'PowerMK - Sistema de Gestión Mary Kay',
+    description: 'Sistema de Gestión Mary Kay para consultoras independientes',
+    siteName: 'PowerMK',
+  },
   icons: {
     icon: '/favicon.ico',
   },
